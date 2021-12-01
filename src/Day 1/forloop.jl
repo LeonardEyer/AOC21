0d8e1233006df5e0013b86f300176b4f50328425
Leonard Eyer <leonardeyer@fastmail.de>
2021-12-01 12:32:06 +0100

    Add docstrings

--- src/Day 1/forloop.jl
+++ src/Day 1/forloop.jl
@@ -1,13 +1,27 @@
 module forLoop
     export count_increasing
     
+    """
+        count_increasing(x)
+
+    Count the number of times a value in x increases from the previous value 
+
+    # Examples
+    ```julia-repl
+    julia> count_increasing([1, 2, 2, 4])
+    2
+    ```
+    """ 
     function count_increasing(input)
+        # Initialize counter and first value to compare
         counter = -1
         last = 0
         for x in input
+            # Everytime we have an increase increment
             if x > last 
                 counter += 1
             end
+            # Remember the last seen value
             last = x
         end
         return counter
