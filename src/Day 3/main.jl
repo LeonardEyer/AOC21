5e5817fb9da299c8d7607e017b5060e7ab08a0d8
Leonard Eyer <leonardeyer@fastmail.de>
2021-12-03 14:18:01 +0100

    Solve Day 3. Part 1

--- src/Day 3/main.jl
+++ src/Day 3/main.jl
@@ -1,6 +1,28 @@
 module Day3
+    using Statistics
+
+    function binary_to_matrix(x)
+        function parse_binary(x)
+            return map(x -> parse(Int, x), collect(x))
+        end
+        binary = collect(Iterators.flatten(map(parse_binary, x)))
+        matrix = reshape(binary, length(x[1]), length(x))
+
+        return matrix
+    end
     function part1(input)
-        return 0
+        # https://discourse.julialang.org/t/parse-an-array-of-bits-bitarray-to-an-integer/42361
+        function bitarr_to_int(arr)
+            return sum(arr .* (2 .^ collect(length(arr)-1:-1:0)))
+        end
+        
+        s = binary_to_matrix(input)
+        a = mean(s, dims=2) .> 0.5
+        b = 1 .- a
+        a = bitarr_to_int(a)
+        b = bitarr_to_int(b)
+        
+        return a * b
     end
     function part2(input)
         return 0
--- test/runtests.jl
+++ test/runtests.jl
@@ -30,4 +30,16 @@ using Test
         end
     end
 
+    @testset "Day3" begin
+        input = ["00100" "11110" "10110" "10111" "10101" "01111" "00111" "11100" "10000" "11001" "00010" "01010"]
+        result_1 = 198
+        result_2 = -1
+
+        @testset "Part 1" begin
+            @test AOC21.Day3.part1(input) == result_1
+        end
+        @testset "Part 2" begin
+            @test AOC21.Day3.part2(input) == result_2
+        end
+    end
 end
