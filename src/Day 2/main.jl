c8d290105c8883f2e4d9082c08fa9df763735e01
Leonard Eyer <leonardeyer@fastmail.de>
2021-12-03 13:05:36 +0100

    Solve day2 part 1

--- src/Day 2/main.jl
+++ src/Day 2/main.jl
@@ -1,6 +1,22 @@
 module Day2
     function part1(input)
-        return 0
+
+        function split_parse(x)
+            a, b = split(x, " ")
+            return [a, parse(Int, b)]
+        end
+
+        function sum_second(prev, x)
+            return prev + x[2]
+        end
+
+        split_input = map(split_parse, input)
+
+        forward = reduce(sum_second, filter(x -> x[1] == "forward", split_input), init=0)
+        up = reduce(sum_second, filter(x -> x[1] == "up", split_input), init=0)
+        down = reduce(sum_second, filter(x -> x[1] == "down", split_input), init=0)
+
+        return forward * (down - up)
     end
     function part2(input)
         return 0
