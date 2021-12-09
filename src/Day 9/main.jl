72e26b956fccd93a6d67791905d713d7d8276271
Leonard Eyer <leonardeyer@fastmail.de>
2021-12-09 16:48:37 +0100

    Refactor

--- src/Day 9/main.jl
+++ src/Day 9/main.jl
@@ -5,15 +5,20 @@ module Day9
     end
 
     function part1(input)
+        # Matrix representation
         A = parse_input(input)
 
+        # Generate CartesianIndices
         R = CartesianIndices(A)
+
+        # Find the upper and lower bounds of the indices
         Ifirst, Ilast = first(R), last(R)
 
         # This will be used to look 1 unit around our current value
         I1 = oneunit(Ifirst)
 
-        out = 0
+        # This is were we accumulate risk values
+        risk_level_sum = 0
         for I in R
             # Define a window (respecting the boundary)
             window = (max(Ifirst, I-I1):min(Ilast, I+I1))
@@ -24,13 +29,15 @@ module Day9
             # If all values are smaller then we can log the risk level
             if ((sum(values_smaller) + 1) / length(window)) == 1.0
                 risk_level = A[I] + 1
-                out += risk_level
+                risk_level_sum += risk_level
             end
         end
-        return out
+        return risk_level_sum
     end
 
     function part2(input)
-        input = parse_input(input)
+        A = parse_input(input)
+        boundary_map = display(A .== 9)
+
     end
 end
