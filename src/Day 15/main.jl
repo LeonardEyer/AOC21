a126d9f17f3303330841038d046855caae2c3850
Leonard Eyer <leonardeyer@fastmail.de>
2021-12-15 10:39:47 +0100

    Remove pseudocode

--- src/Day 15/main.jl
+++ src/Day 15/main.jl
@@ -55,32 +55,6 @@ module Day15
     end
 
     function djikstra(G::Graph, start::GraphNode, target::GraphNode)
-        """
-        dist[source] ← 0                           // Initialization
-3
-4      create vertex priority queue Q
-5
-6      for each vertex v in Graph:          
-7          if v ≠ source
-8              dist[v] ← INFINITY                 // Unknown distance from source to v
-9              prev[v] ← UNDEFINED                // Predecessor of v
-10
-11         Q.add_with_priority(v, dist[v])
-12
-13
-14     while Q is not empty:                      // The main loop
-15         u ← Q.extract_min()                    // Remove and return best vertex
-           if u == target
-                return
-16         for each neighbor v of u:              // only v that are still in Q
-17             alt ← dist[u] + length(u, v)
-18             if alt < dist[v]
-19                 dist[v] ← alt
-20                 prev[v] ← u
-21                 Q.decrease_priority(v, alt)
-22
-23     return dist, prev
-        """
         start.distance = 0
 
         Q = PriorityQueue{GraphNode, Float16}()
